0dc456778b00289a13de002dda5481ee5ea755cd
Sn0w <garrettbeasley@icloud.com>
2014-09-24 22:52:00 -0700

    Init Commit of Project

---
+++ src/House.java
@@ -0,0 +1,22 @@
+/**
+ * House.java
+ * @author Garrett J. Beasley
+ * 09/29/2014
+ * Draws a House Picture
+ */
+
+//Imports for the program.
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+import java.awt.Color;
+
+public class House 
+{
+	public void run()
+	{
+		
+		
+		
+	}
+
+}
