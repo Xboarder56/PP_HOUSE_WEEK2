f8e2f95771430e3c9a7577bf2e2a72752e956e60
Sn0w <garrettbeasley@icloud.com>
2014-09-25 12:32:26 -0700

    Update the class for adding the GRec for the project and ints

--- src/House.java
+++ src/House.java
@@ -10,11 +10,26 @@ import acm.graphics.*;
 import acm.program.GraphicsProgram;
 import java.awt.Color;
 
-public class House 
+public class House extends GraphicsProgram 
 {
 	public void run()
 	{
 		
+		//Variables for the project.
+		/**Values for the rect start points*/
+		int startX = 100;
+		int startY = 100;
+		/**Values for the window Size*/
+		int windowX = 400;
+		int windowY = 300;
+		
+		//Sets the size of the window for the program
+		 setSize(windowX, windowY);
+		
+		
+		GRect rec = new GRect(50, 50);
+		rec.setLocation(startX, startY);
+		add(rec);
 		
 		
 	}
